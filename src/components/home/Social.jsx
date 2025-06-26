55d94e7bbe88a31f4677e7ba2f6f2eba6cb045c2
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-06-26 14:36:57 +0530

    Added Home section with responsive layout

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+        <a href="https://www.linkedin.com" className="home__social-icon" target='blank'>
+            <i className="uil uil-linkedin"></i>
+        </a>
+        <a href="https://www.linkedin.com" className="home__social-icon" target='blank'>
+            <i className="uil uil-github"></i>
+        </a>
+        <a href="https://www.instagram.com" className="home__social-icon" target='blank'>
+            <i className="uil uil-instagram"></i>
+        </a>
+        <a href="https://www.facebook.com" className="home__social-icon" target='blank'>
+            <i className="uil uil-facebook"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
