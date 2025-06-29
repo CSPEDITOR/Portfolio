e2a616f79544bb2615661bef3e58de50859d6fbd
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-06-29 13:27:01 +0530

    Updated Social Links in Social page

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Social = () => {
   return (
     <div className='home__social'>
-        <a href="https://www.linkedin.com" className="home__social-icon" target='blank'>
+        <a href="https://www.linkedin.com/in/jagnyadattadalai/" className="home__social-icon" target='blank'>
             <i className="uil uil-linkedin"></i>
         </a>
-        <a href="https://www.linkedin.com" className="home__social-icon" target='blank'>
+        <a href="https://github.com/jagnyadatta" className="home__social-icon" target='blank'>
             <i className="uil uil-github"></i>
         </a>
-        <a href="https://www.instagram.com" className="home__social-icon" target='blank'>
+        <a href="https://www.instagram.com/ig_jagnyadatta/" className="home__social-icon" target='blank'>
             <i className="uil uil-instagram"></i>
         </a>
-        <a href="https://www.facebook.com" className="home__social-icon" target='blank'>
+        <a href="https://www.facebook.com/jagnyadatta.dalai.5/" className="home__social-icon" target='blank'>
             <i className="uil uil-facebook"></i>
         </a>
     </div>
