c87cf3f6d38da5d60a546cc3b5dfcdfc134f3f96
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-07-05 15:56:11 +0530

    Added Portfolio section with responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import Qualification from './components/qualification/Qualification'
+import Work from './components/work/Work'
 import ScrollUp from './components/scrollup/ScrollUp'
 import Services from './components/services/Services'
 import Skills from './components/skills/Skills'
@@ -20,6 +21,7 @@ function App() {
         <Skills/> 
         <Services/>
         <Qualification/>
+        <Work/>
         {/* <Testimonials/> */}
         <Contact/>
       </main>
